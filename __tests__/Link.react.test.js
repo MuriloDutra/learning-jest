83550baed70acb65493916ef06532e712156c4b1
MuriloDutraProjects <MuriloDutraProjects@gmail.com>
2021-01-26 10:28:52 -0300

    Snapshots module - done

--- __tests__/Link.react.test.js
+++ __tests__/Link.react.test.js
@@ -1,25 +1,41 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import Link from '../src/Link'
+import React from "react";
+import renderer from "react-test-renderer";
+import Link from "../src/Link";
 
+it("renders correctly", () => {
+  const tree = renderer
+    .create(<Link page="http://www.instagram.com">Instagram</Link>)
+    .toJSON();
 
-it('renders correctly', () => {
-    const tree = renderer
-        .create(<Link page="http://www.instagram.com">Instagram</Link>)
-        .toJSON()
+  expect(tree).toMatchSnapshot();
+});
 
-    expect(tree).toMatchSnapshot()
-})
+it("Inline snapshot", () => {
+  const tree = renderer
+    .create(<Link page="http://www.instagram.com">Instagram</Link>)
+    .toJSON();
 
-/*it('will check the matchers and pass 1', () => {
+  expect(tree).toMatchInlineSnapshot(`
+    <a
+      className="normal"
+      href="http://www.facebook.com"
+      onMouseEnter={[Function]}
+      onMouseLeave={[Function]}
+    >
+      Facebook
+    </a>
+  `);
+});
+
+it('will fail every time', () => {
     const user = {
         createdAt: new Date(),
         id: Math.floor(Math.random() * 20),
         name: 'LeBron James',
     };
-
+  
     expect(user).toMatchSnapshot({
         createdAt: expect.any(Date),
-        id: expect.any(Number),
-    })
-})*/
+        id: expect.any(Number)
+    });
+  });
