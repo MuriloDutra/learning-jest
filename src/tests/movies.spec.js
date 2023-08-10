da99e306693b9145ff7863cd5f4b9e1c25054a48
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2023-08-10 17:21:51 -0300

    Using beforeEach hook

---
+++ src/tests/movies.spec.js
@@ -0,0 +1,23 @@
+import movies from "../resources/movies";
+
+describe("Favorite Movies", () => {
+  let myMovies = [];
+  beforeEach(() => {
+    myMovies = [
+      {
+        title: "Age of Ultron",
+        rate: null,
+      },
+    ];
+  });
+
+  test("Can add a movie", () => {
+    movies.add(myMovies, "Avatar");
+    expect(myMovies).toMatchSnapshot();
+  });
+
+  test("Rate a movie", () => {
+    movies.rate(myMovies[0], 5);
+    expect(myMovies).toMatchSnapshot();
+  });
+});
